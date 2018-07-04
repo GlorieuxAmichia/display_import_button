014bc450ce11508132831dd2c90e252f7413becf
AKA <arnaud.aka@smile.ci>
2018-07-04 01:45:25 +0000

    Modification du fonctionnement du module

--- display_import_button/static/src/js/tools.js
+++ display_import_button/static/src/js/tools.js
@@ -7,7 +7,7 @@ var Tools = AbstractView.include({
     init: function (viewInfo, params) {
         this._super(viewInfo, params);
         // Importation option on views
-        var importation = viewInfo.arch.attrs.import ? JSON.parse(viewInfo.arch.attrs.import) : false;
+        var importation = viewInfo.arch.attrs.import ? JSON.parse(viewInfo.arch.attrs.import) : true;
         this.controllerParams.activeActions['import'] = importation;
     },
     });

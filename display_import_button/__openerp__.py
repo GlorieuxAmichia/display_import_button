ebc2c745b340701aab5b1b73173b930ba72d23c3
AKA <arnaud.aka@smile.ci>
2018-08-30 02:39:35 +0000

    Push on Git of Version 9.0 on Import Button Attribute

--- display_import_button/__openerp__.py
+++ display_import_button/__openerp__.py
@@ -2,18 +2,17 @@
 {
 	'name': "Remove Import Button",
 	'sequence': 0,
-	'summary': """Use To Display or Not Import Button on Your Tree and Kanban Views""",
+	'summary': """Use To Display or Not Import Button on Your Tree Views""",
 	'description': """
 		This module is used to manage the display of the "import" button on your
-		list, form, kanban view, according to your needs.
+		tree view, according to your needs.
 	""",
-	'author': "AKA AMICHIA FREJUS ARNAUD",
+	'author': "SLife Organization, Odoo Community Association (OCA)",
 	'category': 'web',
 	'version': '1.0',
 	'license': 'AGPL-3',
-	'depends': ['web'],
+	'depends': ['base_import'],
 	'images': [
-		'static/src/img/main_1.png',
 		'static/src/img/main_screenshot.png'
 	],
 	'qweb': ['static/src/xml/template.xml'],

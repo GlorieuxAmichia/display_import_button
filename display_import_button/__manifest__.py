ea2e2252fcfa0610b8acf415c360a12853be86ac
AKA <arnaud.aka@smile.ci>
2018-09-11 10:29:27 +0000

    Change the author line

--- display_import_button/__manifest__.py
+++ display_import_button/__manifest__.py
@@ -7,7 +7,7 @@
 		This module is used to manage the display of the "import" button on your
 		list, form, kanban view, according to your needs.
 	""",
-	'author': "SLife Organization, Odoo Community Association (OCA)",
+	'author': "SLife Organization",
 	'category': 'web',
 	'version': '1.0',
 	'license': 'AGPL-3',
